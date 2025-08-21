745a333468b346b9f10a70da4723ea6a60131ca2
eun0115 <gianpaoloestacio5@gmail.com>
2025-08-21 15:26:01 +0800

    AK3: add support for oneui 7
    Signed-off-by: eun0115 <gianpaoloestacio5@gmail.com>

---
+++ anykernel.sh
@@ -0,0 +1,106 @@
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
+
+### AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=Cartel Kernel by eun0115
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=a71
+device.name2=
+device.name3=
+device.name4=
+device.name5=
+supported.versions=
+supported.patchlevels=
+'; } # end properties
+
+### AnyKernel install
+# begin attributes
+attributes() {
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+} # end attributes
+
+
+## boot shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
+
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh && attributes;
+
+# boot install
+dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+
+new=$(file_getprop /system/build.prop ro.build.version.oneui)
+legacy=$(file_getprop /system/build.prop ro.build.version.sdk)
+
+if [ -n "$legacy" ] && [ "$legacy" -lt 34 ]; then
+    ui_print "OneUI Legacy detected!"
+    cp -f "$home/OneUI-Legacy-Image.gz-dtb" "$home/Image.gz-dtb"
+elif [ -n "$new" ]; then
+    ui_print "OneUI New detected!"
+    cp -f "$home/OneUI-New-Image.gz-dtb" "$home/Image.gz-dtb"
+else
+    ui_print "AOSP detected!"
+    cp -f "$home/AOSP-Image.gz-dtb" "$home/Image.gz-dtb"
+fi
+
+write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+## end boot install
+
+
+## init_boot shell variables
+#block=init_boot;
+#is_slot_device=1;
+#ramdisk_compression=auto;
+#patch_vbmeta_flag=auto;
+
+# reset for init_boot patching
+#reset_ak;
+
+# init_boot install
+#dump_boot; # unpack ramdisk since it is the new first stage init ramdisk where overlay.d must go
+
+#write_boot;
+## end init_boot install
+
+
+## vendor_kernel_boot shell variables
+#block=vendor_kernel_boot;
+#is_slot_device=1;
+#ramdisk_compression=auto;
+#patch_vbmeta_flag=auto;
+
+# reset for vendor_kernel_boot patching
+#reset_ak;
+
+# vendor_kernel_boot install
+#split_boot; # skip unpack/repack ramdisk, e.g. for dtb on devices with hdr v4 and vendor_kernel_boot
+
+#flash_boot;
+## end vendor_kernel_boot install
+
+
+## vendor_boot shell variables
+#block=vendor_boot;
+#is_slot_device=1;
+#ramdisk_compression=auto;
+#patch_vbmeta_flag=auto;
+
+# reset for vendor_boot patching
+#reset_ak;
+
+# vendor_boot install
+#dump_boot; # use split_boot to skip ramdisk unpack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
+
+#write_boot; # use flash_boot to skip ramdisk repack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
+## end vendor_boot install
+
