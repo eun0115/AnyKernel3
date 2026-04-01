15cfd1b258d81ac32f679a6cf47d3c1a8ab28aab
Gian Paolo <gianpaoloestacio5@gmail.com>
2026-04-01 23:20:43 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=回 Kernel For Redmi Note 10 Pro/Pro Max/12 Pro 4G
+kernel.string=Cartel Kernel by eun0115
 do.devicecheck=1
 do.modules=0
 do.systemless=1
